a2b6bce5a8984db460e2a577a59419056e74e71a
AnaHonrubia <honrubiaborjaa@gmail.com>
2026-03-19 12:39:01 +0100

    Mi Pagina Web

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonCard, IonCardHeader, IonCardTitle, 
+         IonCardSubtitle, IonCardContent, IonItem, IonLabel, IonList, IonIcon, IonGrid, IonRow, IonCol
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonCard, IonCardHeader, IonCardTitle, 
+            IonCardSubtitle, IonCardContent, IonItem, IonLabel, IonList, IonIcon, IonGrid, IonRow, IonCol
+  ],
 })
+
 export class HomePage {
   constructor() {}
+
+  scrollTo(id: string) {
+    const el = document.getElementById(id);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  openCV() {
+    window.open('assets/CV_AHB.pdf', '_blank');
+  }
 }
