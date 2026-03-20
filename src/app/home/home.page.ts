89041f05318286cd54a3744049f2bddf2f2e3d82
AnaHonrubia <honrubiaborjaa@gmail.com>
2026-03-20 11:22:02 +0100

    Añadir habilidades y contacto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,7 +4,9 @@ import {
   IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, 
   IonItem, IonLabel, IonList, IonIcon, IonGrid, IonBadge, IonRow, IonAvatar, IonImg, IonCol
 } from '@ionic/angular/standalone';
-import { logoLinkedin, mailOutline, logoGithub } from 'ionicons/icons';
+import { logoLinkedin, mailOutline, logoGithub, logoHtml5, logoCss3, logoJavascript, 
+         logoAngular, logoIonic, logoFirebase, logoAndroid, logoInstagram, codeSlashOutline,
+         gitBranchOutline, cafeOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 
 @Component({
@@ -21,7 +23,18 @@ import { addIcons } from 'ionicons';
 
 export class HomePage {
   constructor() {
-    addIcons({ logoLinkedin, mailOutline, logoGithub });
+    addIcons({ logoLinkedin, mailOutline, logoGithub, 
+               'logo-html5': logoHtml5, 
+               'logo-css3': logoCss3, 
+               'logo-javascript': logoJavascript, 
+               'logo-angular': logoAngular, 
+               'logo-ionic': logoIonic, 
+               'logo-firebase': logoFirebase, 
+               'logo-android': logoAndroid, 
+               'logo-instagram': logoInstagram,
+               'code-slash-outline': codeSlashOutline,
+               'git-branch-outline': gitBranchOutline,
+               'logo-java': cafeOutline });
   }
 
   scrollTo(id: string) {
