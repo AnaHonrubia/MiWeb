041ff85547e8a66ef93cd3bf026b0d9cbf3bf59f
AnaHonrubia <honrubiaborjaa@gmail.com>
2026-03-26 09:39:53 +0100

    feat(icons): registrar iconos locales en componentes independientes(proyectos)

---
+++ src/app/components/proyectos/proyectos.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { 
+  IonGrid, IonRow, IonCol, IonCard, IonCardHeader, 
+  IonCardTitle, IonCardSubtitle, IonCardContent, IonBadge, IonButton, IonIcon 
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logoGithub } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-proyectos',
+  templateUrl: './proyectos.component.html',
+  styleUrls: ['./proyectos.component.scss'],
+  standalone: true,
+  imports: [
+    IonGrid, IonRow, IonCol, IonCard, IonCardHeader, 
+    IonCardTitle, IonCardSubtitle, IonCardContent, IonBadge, IonButton, IonIcon
+  ]
+})
+export class ProyectosComponent {
+  constructor() {
+    addIcons({ logoGithub });
+  }
+}
