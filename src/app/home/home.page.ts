9348ad67be50194f00f5b65f1c765e0ce3da59a7
AnaHonrubia <honrubiaborjaa@gmail.com>
2026-03-26 09:14:02 +0100

    feat: limpiar la lógica de home e importar los componentes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { 
-  IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, 
-  IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, 
-  IonItem, IonLabel, IonList, IonIcon, IonGrid, IonBadge, IonRow, IonAvatar, IonImg, IonCol
+  IonHeader, IonToolbar, IonContent, IonButton, IonButtons, IonAvatar, IonImg 
 } from '@ionic/angular/standalone';
-import { logoLinkedin, mailOutline, logoGithub, logoHtml5, logoCss3, logoJavascript, 
-         logoAngular, logoIonic, logoFirebase, logoAndroid, logoInstagram, codeSlashOutline,
-         gitBranchOutline, cafeOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { logoLinkedin, mailOutline, logoGithub } from 'ionicons/icons';
+
+// Importa tus nuevos componentes aquí:
+import { SobreMiComponent } from '../components/sobre-mi/sobre-mi.component';
+import { FormacionComponent } from '../components/formacion/formacion.component';
+import { ProyectosComponent } from '../components/proyectos/proyectos.component';
+import { ExperienciaComponent } from '../components/experiencia/experiencia.component';
+import { HabilidadesComponent } from '../components/habilidades/habilidades.component';
+import { ContactoComponent } from '../components/contacto/contacto.component';
 
 @Component({
   selector: 'app-home',
@@ -15,26 +19,21 @@ import { addIcons } from 'ionicons';
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [
-      IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, 
-      IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, 
-      IonItem, IonLabel, IonList, IonIcon, IonGrid, IonBadge, IonRow, IonAvatar, IonImg, IonCol
+    // Componentes de Ionic necesarios para la Home
+    IonHeader, IonToolbar, IonContent, IonButton, IonButtons, IonAvatar, IonImg,
+    // Tus componentes personalizados
+    SobreMiComponent,
+    FormacionComponent,
+    ProyectosComponent,
+    ExperienciaComponent,
+    HabilidadesComponent,
+    ContactoComponent
   ],
 })
-
 export class HomePage {
   constructor() {
-    addIcons({ logoLinkedin, mailOutline, logoGithub, 
-               'logo-html5': logoHtml5, 
-               'logo-css3': logoCss3, 
-               'logo-javascript': logoJavascript, 
-               'logo-angular': logoAngular, 
-               'logo-ionic': logoIonic, 
-               'logo-firebase': logoFirebase, 
-               'logo-android': logoAndroid, 
-               'logo-instagram': logoInstagram,
-               'code-slash-outline': codeSlashOutline,
-               'git-branch-outline': gitBranchOutline,
-               'logo-java': cafeOutline });
+    // En la Home solo registramos los iconos que se ven en la cabecera/hero
+    addIcons({ logoLinkedin, mailOutline, logoGithub });
   }
 
   scrollTo(id: string) {
