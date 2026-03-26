bb1a0be77f4689cd645054140a896334745a9e5b
AnaHonrubia <honrubiaborjaa@gmail.com>
2026-03-26 09:32:32 +0100

    feat(about): habilita el modo independiente e importa módulos de Ionic

---
+++ src/app/components/sobre-mi/sobre-mi.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+// Importamos solo lo que usa este componente
+import { 
+  IonGrid, IonRow, IonCol, IonCard, IonCardContent, IonAvatar 
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-sobre-mi',
+  templateUrl: './sobre-mi.component.html',
+  styleUrls: ['./sobre-mi.component.scss'],
+  standalone: true, // Muy importante para que funcione solo
+  imports: [
+    IonGrid, 
+    IonRow, 
+    IonCol, 
+    IonCard, 
+    IonCardContent, 
+    IonAvatar
+  ]
+})
+export class SobreMiComponent {
+  constructor() { }
+}
