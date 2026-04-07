85e111c8f6921b47b85898b73635b81b071d6386
AnaHonrubia <honrubiaborjaa@gmail.com>
2026-04-07 17:13:00 +0200

    refactor: usar molecula card-experiencia en la sección (componente) experiencia

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/angular/standalone';
+import { Component } from '@angular/core';
+import { CardExperienciaComponent } from '../molecules/card-experiencia/card-experiencia.component';
 import { TituloComponent } from '../atoms/titulo/titulo.component';
 
 @Component({
@@ -7,12 +7,8 @@ import { TituloComponent } from '../atoms/titulo/titulo.component';
   templateUrl: './experiencia.component.html',
   styleUrls: ['./experiencia.component.scss'],
   standalone: true,
-  imports: [ IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, TituloComponent  ]
+  imports: [CardExperienciaComponent, TituloComponent]
 })
-export class ExperienciaComponent  implements OnInit {
-
+export class ExperienciaComponent {
   constructor() { }
-
-  ngOnInit() {}
-
 }
