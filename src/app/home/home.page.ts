0cd25ef15337f9205d05904853cd4545ddcf9b36
AnaHonrubia <honrubiaborjaa@gmail.com>
2026-04-07 14:13:59 +0200

    refactor: simplificar cabecera de la home usando app-navbar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
-import { 
-  IonHeader, IonToolbar, IonContent, IonButton, IonButtons, IonAvatar, IonImg 
-} from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import { logoLinkedin, mailOutline, logoGithub } from 'ionicons/icons';
+import { IonContent } from '@ionic/angular/standalone';
 
-// Importación de tus componentes Standalone
+// Importación de tus componentes modulares y organismos
+import { NavbarComponent } from '../components/organisms/navbar/navbar.component';
+import { HeroComponent } from '../components/organisms/hero/hero.component';
 import { SobreMiComponent } from '../components/sobre-mi/sobre-mi.component';
 import { FormacionComponent } from '../components/formacion/formacion.component';
 import { ProyectosComponent } from '../components/proyectos/proyectos.component';
 import { ExperienciaComponent } from '../components/experiencia/experiencia.component';
 import { HabilidadesComponent } from '../components/habilidades/habilidades.component';
 import { ContactoComponent } from '../components/contacto/contacto.component';
-import { BotonComponent } from '../components/atoms/boton/boton.component';
-import { AvatarComponent } from '../components/atoms/avatar/avatar.component';
-import { HeroComponent } from '../components/organisms/hero/hero.component';
 
 @Component({
   selector: 'app-home',
@@ -22,17 +17,22 @@ import { HeroComponent } from '../components/organisms/hero/hero.component';
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [
-    // Componentes nativos de Ionic para la estructura principal
-    IonHeader, IonToolbar, IonContent, IonButton, IonButtons, IonAvatar, IonImg,
-    // Tus componentes modulares
-    SobreMiComponent, FormacionComponent, ProyectosComponent, ExperienciaComponent, HabilidadesComponent, ContactoComponent,
-    BotonComponent, AvatarComponent, HeroComponent
+    // Solo necesitamos IonContent para el scroll principal
+    IonContent,
+    // Tus nuevos organismos
+    NavbarComponent,
+    HeroComponent,
+    // Tus secciones modulares
+    SobreMiComponent,
+    FormacionComponent,
+    ProyectosComponent,
+    ExperienciaComponent,
+    HabilidadesComponent,
+    ContactoComponent
   ],
 })
 export class HomePage {
-  constructor() {
-    addIcons({ logoLinkedin, mailOutline, logoGithub });
-  }
+  constructor() {}
 
   scrollTo(id: string) {
     const el = document.getElementById(id);
