0a288e0c6abc92b7c8bae36e06384f96ad632f8d
AnaHonrubia <honrubiaborjaa@gmail.com>
2026-04-07 17:49:45 +0200

    cambiar para que aparezcan los 2 iconos que faltaban de las habilides

--- src/app/components/atoms/item-habilidad/item-habilidad.component.ts
+++ src/app/components/atoms/item-habilidad/item-habilidad.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import * as icons from 'ionicons/icons';
+import { 
+  logoHtml5, logoCss3, logoJavascript, logoAngular, logoIonic, 
+  logoGithub, codeSlashOutline 
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-item-habilidad',
@@ -10,12 +13,21 @@ import * as icons from 'ionicons/icons';
   standalone: true,
   imports: [IonIcon]
 })
+
 export class ItemHabilidadComponent {
   @Input() nombre: string = '';
   @Input() icono: string = '';
   @Input() colorIcono: string = '';
 
   constructor() {
-    addIcons({ ...icons });
+    addIcons({ 
+      'logo-html5': logoHtml5, 
+      'logo-css3': logoCss3, 
+      'logo-javascript': logoJavascript, 
+      'logo-angular': logoAngular, 
+      'logo-ionic': logoIonic, 
+      'logo-github': logoGithub,
+      'code-slash-outline': codeSlashOutline
+    });
   }
 }
