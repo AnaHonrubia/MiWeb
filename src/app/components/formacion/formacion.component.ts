a7b73b0f4b2e584704b7f412d074816e5328f264
AnaHonrubia <honrubiaborjaa@gmail.com>
2026-04-07 11:07:11 +0200

    refactor: usar el atomo de titulo en la sección formación

--- src/app/components/formacion/formacion.component.ts
+++ src/app/components/formacion/formacion.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { TituloComponent } from '../atoms/titulo/titulo.component';
 
 @Component({
   selector: 'app-formacion', // Corregido
   templateUrl: './formacion.component.html',
   styleUrls: ['./formacion.component.scss'],
   standalone: true,
-  imports: [IonList, IonItem, IonLabel]
+  imports: [IonList, IonItem, IonLabel, TituloComponent]
 })
 export class FormacionComponent { 
   
