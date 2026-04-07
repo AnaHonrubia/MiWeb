689a5042d917fcc59ea40ccab26b29c6b95f6905
AnaHonrubia <honrubiaborjaa@gmail.com>
2026-04-07 11:05:08 +0200

    refactor: usar el atomo de titulo en la sección experiencia

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/angular/standalone';
+import { TituloComponent } from '../atoms/titulo/titulo.component';
 
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
   styleUrls: ['./experiencia.component.scss'],
   standalone: true,
-  imports: [ IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent  ]
+  imports: [ IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, TituloComponent  ]
 })
 export class ExperienciaComponent  implements OnInit {
 
