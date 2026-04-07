b3196a7d6f39aabf3132d02335116cb24dc1e01b
AnaHonrubia <honrubiaborjaa@gmail.com>
2026-04-07 17:22:35 +0200

    refactor: aplicar molecula item-formacion y eliminar estilos inline

--- src/app/components/formacion/formacion.component.ts
+++ src/app/components/formacion/formacion.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { IonList } from '@ionic/angular/standalone';
 import { TituloComponent } from '../atoms/titulo/titulo.component';
+import { ItemFormacionComponent } from '../molecules/item-formacion/item-formacion.component';
 
 @Component({
-  selector: 'app-formacion', // Corregido
+  selector: 'app-formacion',
   templateUrl: './formacion.component.html',
   styleUrls: ['./formacion.component.scss'],
   standalone: true,
-  imports: [IonList, IonItem, IonLabel, TituloComponent]
+  imports: [IonList, TituloComponent, ItemFormacionComponent]
 })
-export class FormacionComponent { 
-  
-}
+export class FormacionComponent { }
